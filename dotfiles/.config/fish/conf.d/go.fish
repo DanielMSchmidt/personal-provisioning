a12906c9b46ffc2413db654f7ee2b6a1b2ce379b
Daniel Schmidt <danielmschmidt92@gmail.com>
2021-04-28 23:59:54 +0200

    remove go bin

--- dotfiles/.config/fish/conf.d/go.fish
+++ dotfiles/.config/fish/conf.d/go.fish
@@ -2,10 +2,10 @@ set -x GOPRIVATE github.com/mesosphere
 set -x GONOPROXY github.com/mesosphere
 set -x GONOSUMDB github.com/mesosphere
 set -x GOPATH /Users/danielschmidt/go
-set -x GOBIN $GOPATH/bin
+# set -x GOBIN $GOPATH/bin
 
 set -x PATH /usr/local/opt/go/libexec/bin $PATH
-set -x PATH $GOBIN $PATH
+# set -x PATH $GOBIN $PATH
 
 contains /usr/local/opt/go/libexec/bin $PATH; or set -Ua PATH /usr/local/opt/go/libexec/bin
-contains $GOBIN $PATH; or set -Ua PATH $GOBIN
+# contains $GOBIN $PATH; or set -Ua PATH $GOBIN
