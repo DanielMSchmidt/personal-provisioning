22bf49350efd06a61af5f9cf04e4042c001e92ac
Daniel Schmidt <danielmschmidt92@gmail.com>
2023-11-06 14:34:37 +0100

    update

--- dotfiles/.config/fish/conf.d/cdktf.fish
+++ dotfiles/.config/fish/conf.d/cdktf.fish
@@ -1,5 +1,5 @@
 # Don't track me
-set -x CHECKPOINT_DISABLE "true"
+set -x CHECKPOINT_DISABLE true
 
 # helpers
 alias cdktfl="/Users/dschmidt/work/hashicorp/terraform-cdk/packages/cdktf-cli/bundle/bin/cdktf"
@@ -8,3 +8,7 @@ alias jestd="node --inspect-brk ./node_modules/jest/bin/jest.js"
 # TFC related
 alias tfc_user_bigdane="cp ~/.terraform.d/credentials.tfrc.bigdane.json ~/.terraform.d/credentials.tfrc.json"
 alias tfc_user_danielschmidt="cp ~/.terraform.d/credentials.tfrc.danielschmidt.json ~/.terraform.d/credentials.tfrc.json"
+
+# Caching layer
+
+set -x CDKTF_EXPERIMENTAL_PROVIDER_SCHEMA_CACHE_PATH "/Users/dschmidt/.cdktf/schema-cache"
