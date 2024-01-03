5388f30890cb25ea1ab1a320103970a856255237
Daniel Schmidt <danielmschmidt92@gmail.com>
2024-01-03 12:11:43 +0100

    chore: add terraform commands

--- dotfiles/.config/fish/conf.d/terraform.fish
+++ dotfiles/.config/fish/conf.d/terraform.fish
@@ -1,1 +1,6 @@
+# Execute local terraform binary
 alias tfl="/Users/dschmidt/work/hashicorp/terraform/bin/terraform"
+# Build terraform binary
+alias tfb="cd /Users/dschmidt/work/hashicorp/terraform && go build -v -o bin/"
+# Watch terraform binary build
+alias tfbw="cd /Users/dschmidt/work/hashicorp/terraform && gow -c build -v -o bin/"
