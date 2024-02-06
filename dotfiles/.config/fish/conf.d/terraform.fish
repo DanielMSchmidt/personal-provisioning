cef611b712e7b0d7296443c94543e5cc405f7b4e
Daniel Schmidt <danielmschmidt92@gmail.com>
2024-02-06 18:26:06 +0100

    chore: improve watch tooling (verbose is great)

--- dotfiles/.config/fish/conf.d/terraform.fish
+++ dotfiles/.config/fish/conf.d/terraform.fish
@@ -3,4 +3,4 @@ alias tfl="/Users/dschmidt/work/hashicorp/terraform/bin/terraform"
 # Build terraform binary
 alias tfb="cd /Users/dschmidt/work/hashicorp/terraform && go build -v -o bin/"
 # Watch terraform binary build
-alias tfbw="cd /Users/dschmidt/work/hashicorp/terraform && gow -c build -v -o bin/"
+alias tfw="cd /Users/dschmidt/work/hashicorp/terraform && gow -v -c build -v -o bin/"
