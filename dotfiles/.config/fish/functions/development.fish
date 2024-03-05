080394c433d77a2e1bc3efbaf223768fa03ff8d5
Daniel Schmidt <danielmschmidt92@gmail.com>
2024-03-05 15:06:25 +0100

    add jira opener

--- dotfiles/.config/fish/functions/development.fish
+++ dotfiles/.config/fish/functions/development.fish
@@ -1,3 +1,7 @@
 function tmpdir -d "Creates a temporary directory and switches into it"
     cd (mktemp -d)
 end
+
+function jira -d "Opens jira for this branch"
+    open "https://hashicorp.atlassian.net/browse/$(git branch --show-current)"
+end
