8470c7a880e5a49bed45255793b0bd5948692703
Daniel Schmidt <danielmschmidt92@gmail.com>
2024-04-12 09:22:10 +0200

    auto-set tab name

--- dotfiles/.config/fish/functions/zellij.fish
+++ dotfiles/.config/fish/functions/zellij.fish
@@ -1,3 +1,11 @@
 function set_tab_name -d "Sets the name of the tab to the dir name"
     zellij action rename-tab (basename (pwd))
 end
+
+# Set on cd
+function __set_tab_name --on-variable PWD --description 'Set name of the tab to the dir name'
+    status --is-command-substitution; and return
+    set_tab_name
+end
+# Set on new tab
+set_tab_name
